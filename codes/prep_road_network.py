1e913aca74752bd967f3b7fa53e04bc6aeff7708
Adelle Arbo <giarbo.28@gmail.com>
2022-04-14 02:27:12 +0200

    add road length and width

--- codes/prep_road_network.py
+++ codes/prep_road_network.py
@@ -4,14 +4,15 @@ Prepare detailed road network
 Road sections as part of the detailed road network of Berlin for traffic purposes, 
 contains other roads and paths in addition to the classified road network.
 """
-
+#%%
 import pandas as pd
 import geopandas as gpd
 import os
 import shapely
-
+#%%
 path = os.getcwd()
 
+#%%
 # Road network detailed
 road_gdf = gpd.read_file(path + '/data/raw/road network/shp/Detailnetz-Strassenabschnitte.shp')
 road_gdf.element_nr.value_counts() # element_nr unique segment id
@@ -19,20 +20,40 @@ road_gdf = road_gdf.reset_index()  # create new segment id
 road_gdf.rename(columns = {'index':'segment_id'}, inplace = True)
 
 road_gdf = road_gdf.to_crs(4326)
+#%%
+# segment length in meters - almost equal to "laenge" variable, confirms in meters
+road_gdf['length_m'] = road_gdf.to_crs(3174).length 
 
 # return the midpoint of a segment
 road_gdf['midpoint'] = road_gdf.centroid 
 
-# segment length in meters - almost equal to "laenge" variable, confirms in meters
-road_gdf['length_m'] = road_gdf.to_crs(3174).length 
-road_shp = road_gdf.to_crs(4326)
+# merge with road width
+road_width = pd.read_csv(path + "/data/output/road_width.csv")
+road_gdf = road_gdf.merge(road_width, how="left", on = "segment_id")
 
+road_shp = road_gdf.to_crs(4326)
+#%%
 # Save
 road_gdf.to_csv(path + "/data/output/road_segments.csv", index = False)
 
-road_shp = road_gdf.to_crs(4326)
-road_shp.drop(['midpoint'], axis=1, inplace=True) # saving to shapefile only takes 1 geom
-road_shp.to_file(path + "/data/output/road_shp.shp", index = False)
+#road_shp = road_gdf.to_crs(4326)
+#road_shp.drop(['midpoint'], axis=1, inplace=True) # saving to shapefile only takes 1 geom
+#road_shp.to_file(path + "/data/output/road_shp.shp", index = False)
+
+
+
+road_shp.drop(['geometry'], axis=1, inplace=True) # saving to shapefile only takes 1 geom
+road_shp = road_shp.set_geometry(road_shp['midpoint'])
+
+road_shp.rename(columns = {'midpoint':'geometry'}, inplace = True)
+road_shp = road_shp.iloc[:,0:19]
+
+road_shp = gpd.GeoDataFrame(road_shp.set_geometry(road_shp['midpoint']))
+
+road_shp.to_file(path + "/data/output/road_shp2.shp", index = False)
+
+###############################
+
 
 # Check midpoints - correct
 #def df_to_gdf(df, projection, geometry):
