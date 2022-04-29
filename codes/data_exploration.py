2f31e3fe80a9690978e0101f01552ce6d21f4338
aneleh-cikab <90176475+aneleh-cikab@users.noreply.github.com>
2022-04-29 22:23:36 +0200

    Data exploration - added plotted segments based on the number of accidents

--- codes/data_exploration.py
+++ codes/data_exploration.py
@@ -4,10 +4,10 @@ Data exploration
 """
 
 import pandas as pd
-# geopandas as gpd
 import os
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import geopandas as gpd
 
 #figure settings
 mpl.rc('axes', labelsize=14)
@@ -22,9 +22,8 @@ pd.set_option('display.max_columns', 10)
 
 #loading data
 path = os.getcwd()
-train = pd.read_csv(path + "/data/train_data_small.csv")
-
-full = pd.read_csv(path + "/data/full_data_small.csv")
+train = pd.read_csv(path + "/data/train_data_big.csv")
+#full = pd.read_csv(path + "/data/full_data_big.csv")
 
 
 #quick look
@@ -54,15 +53,30 @@ cor_matrix = train.corr()
 cor_matrix["collision"].sort_values(ascending=False)
 
 #histograms
-best = ["collision", "accident_no", "sun_elevation_angle", "temperature", "humidity", "hour_cos"]
+best = ["accident_no", "sun_elevation_angle", "temperature", "humidity", "hour_cos"]
 
 train.hist(column=best)
 plt.show()
+plt.close()
 
-#scatter matrix
-scatter_matrix(train[best], figsize=(12, 8))
+#segment plot
+segments = pd.read_csv(path + "/data/road_segments.csv")
+segments_plot = segments.merge(accident_segment[["segment_id", "accident_no"]], left_on="segment_id", right_on="segment_id", how="left")
+
+from shapely import wkt
+
+segments_plot['geometry'] = segments_plot['geometry'].apply(wkt.loads)
+segments_gdf = gpd.GeoDataFrame(segments_plot, crs='epsg:3174')
+
+max = segments_gdf.nlargest(300, "accident_no")
+
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(15,15))
+axs[0].set_title("Road segments with the highest number of accidents")
+axs[1].set_title("Road segments with no accidents")
+segments_gdf.nlargest(1500, "accident_no").plot(ax=axs[0], color="red", markersize=0.2, alpha=0.3)
+segments_gdf.nsmallest(1500, "accident_no").plot(ax=axs[1], color="blue", markersize=0.2, alpha=0.3)
 plt.show()
-
+plt.close()
 
 
 
